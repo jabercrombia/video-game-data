2d81c6a1b40eaf1bee69f82dc2df4b258620a3c3
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-22 00:11:52 -0500

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend to access API
+    allow_origins=["http://localhost:3000","https://video-game-data.vercel.app/"],  # Allow frontend to access API
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
