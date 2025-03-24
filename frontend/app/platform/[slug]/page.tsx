57cec57dbd2938c1789e05c7d8046c153e7d1fbf
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-24 09:53:04 -0500

    slug

--- frontend/app/platform/[slug]/page.tsx
+++ frontend/app/platform/[slug]/page.tsx
@@ -1,9 +1,53 @@
+import { columns } from "../../components/platform/columns"
+import { DataTable } from "../../components/platform/table"
+import ChartBar from "../../components/platform/chart"
+import Pie from "../../components/platform/pie"
 interface PlatformPageProps {
   params: Promise<{ slug: string }>;
 }
 
 export default async function PlatformPage({ params }: PlatformPageProps) {
   const { slug } = await params;
+  
 
-  return <div>Slug: {slug}</div>;
+  // Fetch data from your API
+  const res = await fetch(`http://localhost:3000/api/platform/${slug}?include=name,na_sales,eu_sales,global_sales,other_sales,jp_sales,genre,publisher&query=global_sales&order=DESC`);
+  
+  if (!res.ok) {
+    return <h1>Error: Failed to fetch data</h1>;
+  }
+
+  // Fetch data from your API
+  const topGames = await fetch(`http://localhost:3000/api/platform/${slug}?include=name,na_sales,eu_sales,global_sales,other_sales,jp_sales&query=global_sales&order=DESC`);
+  
+  if (!topGames.ok) {
+    return <h1>Error: Failed to fetch data</h1>;
+  }
+
+  const data = await res.json();
+
+  const dataTopGames = await topGames.json();
+
+  return (
+    <div className="w-full mx-auto px-[30px]">
+      <h1 className="uppercase text-3xl">{slug} Sales</h1>
+
+     
+        <DataTable columns={columns} data={data} />
+        
+        <div className="flex"> 
+          <div className="w-3/4">
+            <ChartBar data={dataTopGames}/>
+          </div>
+          
+
+          <div className="w-1/4">
+            <Pie />
+          </div>
+        </div>
+
+  
+      
+    </div>
+  );
 }
