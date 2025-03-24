b865e9d2bff440b00726fb145d53148c4ec94875
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-23 23:14:14 -0500

    platform

--- frontend/app/platform/[slug]/page.tsx
+++ frontend/app/platform/[slug]/page.tsx
@@ -1,7 +1,4 @@
-import { columns } from "../../components/platform/columns"
-import { DataTable } from "../../components/platform/table"
-import ChartBar from "../../components/platform/chart"
-import Pie from "../../components/platform/pie"
+
 interface PlatformPageParams {
   slug: string;
 }
