0bee0803b83b1703a8d3d684e5f680c7b4e116b0
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-23 23:12:57 -0500

    test

--- frontend/app/platform/[slug]/page.tsx
+++ frontend/app/platform/[slug]/page.tsx
@@ -6,43 +6,6 @@ interface PlatformPageParams {
   slug: string;
 }
 
-export default async function PlatformPage({ params }: { params: PlatformPageParams }) {
-  const { slug } = params; // Extract slug from URL params
-
-  // Fetch data from your API
-  const data = await fetch(`http://localhost:3000/api/platform/${slug}?include=name,na_sales,eu_sales,global_sales,other_sales,jp_sales,genre,publisher&query=global_sales&order=DESC`).then((res) => res.json());
-  
-  if (!data.ok) {
-    return <h1>Error: Failed to fetch data</h1>;
-  }
-
-  // Fetch data from your API
-  const topGames = await fetch(`http://localhost:3000/api/platform/${slug}?include=name,na_sales,eu_sales,global_sales,other_sales,jp_sales&query=global_sales&order=DESC`).then((res) => res.json());
-  
-  if (!topGames.ok) {
-    return <h1>Error: Failed to fetch data</h1>;
-  }
-
-  return (
-    <div className="w-full mx-auto px-[30px]">
-      <h1 className="uppercase text-3xl">{slug} Sales</h1>
-
-     
-        <DataTable columns={columns} data={data} />
-        
-        <div className="flex"> 
-          <div className="w-3/4">
-            <ChartBar data={topGames}/>
-          </div>
-          
-
-          <div className="w-1/4">
-            <Pie />
-          </div>
-        </div>
-
-  
-      
-    </div>
-  );
+export default function PlatformPage({ params }: { params: PlatformPageParams }) {
+  return <div>Slug: {params.slug}</div>;
 }
